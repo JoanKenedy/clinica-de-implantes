dce8409a38619cd7107f37f9b49efa6daab1ea0c
leonelvalentinn <leonelvalentinn@gmail.com>
2023-10-25 15:31:46 -0600

    Testimonios

--- js/swipers.js
+++ js/swipers.js
@@ -1,13 +1,17 @@
 const swiper = new Swiper('.swiper', {
   // Optional parameters
-  direction: 'vertical',
+  // Default parameters
+  slidesPerView: 1,
+  spaceBetween: 10,
+  direction: 'horizontal',
   loop: true,
-
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
   },
-
+  autoplay: {
+    delay: 2000,
+  },
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
@@ -18,4 +22,14 @@ const swiper = new Swiper('.swiper', {
   scrollbar: {
     el: '.swiper-scrollbar',
   },
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+  }
 });
+
+
